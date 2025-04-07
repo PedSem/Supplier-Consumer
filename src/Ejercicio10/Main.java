aa7c8c736b7f7f008058a47316e9651dfddf9750
PedSemp <pedguisem@alu.edu.gva.es>
2025-04-07 08:39:53 +0200

    Consumer Supplier

--- src/Ejercicio10/Main.java
+++ src/Ejercicio10/Main.java
@@ -8,7 +8,7 @@ public class Main {
     public static void main(String[] args) {
         Map<Integer,String>integerStringMap=new LinkedHashMap<>();
         BiConsumer<Integer,String>integerStringBiConsumer=(x,y)-> System.out.println("Clave " + x + " Palabra " + y);
-         integerStringMap.put(1,"hola");
+         integerStringMap.put(1,"Hola");
          integerStringMap.put(2,"Avion");
          integerStringMap.put(3,"Barco");
          integerStringMap.put(4,"Coche");
