0fbf922a1befd10fef946cda1fbcec0b7fc06089
PedSemp <pedguisem@alu.edu.gva.es>
2025-04-07 08:32:37 +0200

    Consumer Supplier

--- src/Ejercicio9/Calculator.java
+++ src/Ejercicio9/Calculator.java
@@ -1,25 +1,19 @@
 package Ejercicio9;
-
 import java.util.function.BiConsumer;
-import java.util.function.BiFunction;
-import java.util.function.Supplier;
-
 public class Calculator {
     public static void main(String[] args) {
-        int a=10;
-        int b=5;
+       int a=-9;
+       int b=7;
         Calculator calculator=new Calculator();
-        int suma=calculator.Calcula(a,b,(x,y)->x+y);
-        int resta=calculator.Calcula(a,b,(x,y)->x-y);
-        int multiplicacion=calculator.Calcula(a,b,(x,y)->x*y);
-        System.out.println(suma);
-        System.out.println(resta);
-        System.out.println(multiplicacion);
-
+        calculator.Calcula(a,b,(x,y)-> System.out.println(x+y));
+        calculator.Calcula(a,b,(x,y)-> System.out.println(x-y));
+        calculator.Calcula(a,b,(x,y)-> System.out.println(x/y));
 
 
     }
-    public int Calcula(int x, int y, BiFunction<Integer,Integer,Integer>opera){
-        return opera.apply(x,y);
+    public void Calcula(int x,int y,BiConsumer<Integer,Integer>calculadora){
+       calculadora.accept(x,y);
+
+
     }
 }
